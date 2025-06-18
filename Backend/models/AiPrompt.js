a73331280769b969dca6897d660708fb2b5879d1
tarun <tarunsharpdeveloper@gmail.com>
2025-06-18 14:29:06 +0530

    fixed query exicute issue

--- Backend/models/AiPrompt.js
+++ Backend/models/AiPrompt.js
@@ -4,28 +4,28 @@ const AiPrompt = {
     create: async (data) => {
         const { prompt_for_id, prompt } = data;
         const query = `INSERT INTO ai_prompts (prompt_for_id, prompt) VALUES ($1, $2)`;
-        const result = await db.query(query, [prompt_for_id, prompt]);
-        return result.rows[0];
+        const [result] = await db.execute(query, [prompt_for_id, prompt]);
+        return result;
     },
     findAll: async () => {
         const query = `SELECT * FROM ai_prompts`;
-        const result = await db.query(query);
-        return result.rows;
+        const [result] = await db.execute(query);
+        return result;
     },
     delete: async (id) => {
         const query = `DELETE FROM ai_prompts WHERE id = $1`;
-        const result = await db.query(query, [id]);
-        return result.rows[0];
+        const [result] = await db.execute(query, [id]);
+        return result;
     },
     getPromptFor: async (prompt_for_id) => {
         const query = `SELECT * FROM ai_prompts WHERE prompt_for_id = $1`;
-        const result = await db.query(query, [prompt_for_id]);
-        return result.rows[0];
+        const [result] = await db.execute(query, [prompt_for_id]);
+        return result;
     },
     getAllPromptsType: async () => {
         const query = `SELECT * FROM ai_prompts_type`;
-        const result = await db.query(query);
-        return result.rows;
+        const [result] = await db.execute(query);
+        return result;
     }
 }
 
