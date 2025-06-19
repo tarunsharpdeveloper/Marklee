de385026582b31037a93fcfbd8b3e0c19bef2240
Atal Joshi <atal.eway@gmail.com>
2025-06-19 10:56:32 +0530

    AIpromptAccepted

--- Backend/models/AiPrompt.js
+++ Backend/models/AiPrompt.js
@@ -1,30 +1,32 @@
 import { pool as db } from "../config/database.js";
 
 class AiPrompt {
-  static async create(data) {
-    const { prompt_for_id, prompt } = data;
-    const query = `INSERT INTO ai_prompts (prompt_for_id, prompt) VALUES (?, ?)`;
-    const [result] = await db.execute(query, [prompt_for_id, prompt]);
-    return result;
-  }
-  static async findAll() {
-    const query = `SELECT * FROM ai_prompts`;
-    const [result] = await db.execute(query);
-    return result;
-  }
-  static async delete(id) {
-    const query = `DELETE FROM ai_prompts WHERE id = ?`;
-    const [result] = await db.execute(query, [id]);
-    return result;
-  }
-  static async getPromptFor(prompt_for_id) {
-    const query = `SELECT * FROM ai_prompts WHERE prompt_for_id = ?`;
-  }
-  static async getAllPromptsType() {
-    const query = `SELECT * FROM ai_prompts_type`;
-    const [result] = await db.execute(query);
-    return result;
-  }
+    static async create(data) {
+        const { prompt_for_id, prompt } = data;
+        const query = `INSERT INTO ai_prompts (prompt_for_id, prompt) VALUES (?, ?)`;
+        const [result] = await db.execute(query, [prompt_for_id, prompt]);
+        return result;
+    }
+    static async findAll() {
+        const query = `SELECT * FROM ai_prompts`;
+        const [result] = await db.execute(query);
+        return result;
+    }
+    static async delete(id) {
+        const query = `DELETE FROM ai_prompts WHERE id = ?`;
+        const [result] = await db.execute(query, [id]);
+        return result;
+    }
+    static async getPromptFor(prompt_for_id) {
+        const query = `SELECT * FROM ai_prompts WHERE prompt_for_id = ?`;
+        const [result] = await db.execute(query, [prompt_for_id]);
+        return result;
+    }
+    static async getAllPromptsType() {
+        const query = `SELECT * FROM ai_prompts_type`;
+        const [result] = await db.execute(query);
+        return result;
+    }
 }
 
-export default AiPrompt;
+export default AiPrompt;  
