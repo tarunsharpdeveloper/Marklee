136fc2beb7e38d4828499e8116467814554f84d8
Tarun Sharma <tarunsharpdeveloper@gmail.com>
2025-07-09 13:03:05 +0530

    IOSuiCheck

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -8,6 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
